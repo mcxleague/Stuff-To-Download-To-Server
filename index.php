0587a3db3a637b364e139e5a8320192178f6028d
MasonX888 <119989048+MasonX888@users.noreply.github.com>
2023-03-19 19:40:36 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,407 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+   <link rel="icon" href="/Assets/favicon.ico" type="image/x-icon">
+    <title>MCX League (US)</title>
+</head>
+
+<body>
+    <div class="nav-container">
+        <nav>
+            <ul class="mobile-nav">
+                <li>
+                    <div class="menu-icon-container">
+                        <div class="menu-icon">
+                            <span class="line-1"></span>
+                            <span class="line-2"></span>
+                        </div>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#" class="link-logo"></a>
+                </li>
+
+                <li>
+                    <a href="" class="link-bag"></a>
+                </li>
+            </ul>
+
+            <ul class="desktop-nav">
+                <li>
+                    <a href="#" class="link-logo"></a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Products</a>
+                </li>
+                <li>
+                    <a href="#">Games</a>
+                </li>
+                <li>
+                    <a href="#">Learn</a>
+                </li>
+                <li>
+                    <a href="#">For Developers</a>
+                </li>
+                <li>
+                    <a href="#">For Education</a>
+                </li>
+                <li>
+                    <a href="#">Support</a>
+                </li>
+                <li>
+                    <a href="#">|</a>
+                </li>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+                <li>
+                    <a href="#" class="link-search"></a>
+                </li>
+                <li>
+                    <a href="#" class="link-bag"></a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- End of navigation menu items -->
+
+        <div class="search-container hide">
+            <div class="link-search"></div>
+            <div class="search-bar">
+                <form action="">
+                    <input type="text" placeholder="Search MCX League">
+                </form>
+            </div>
+            <div class="link-close"></div>
+
+            <div class="quick-links">
+                <h2>Quick Links</h2>
+                <ul>
+                    <li>
+                        <a href="#">MCX League FAQ</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop MCX League Store Online</a>
+                    </li>
+                    <li>
+                        <a href="#">Developer Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Learn</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Support</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="mobile-search-container">
+            <div class="link-search"></div>
+            <div class="search-bar">
+                <form action="">
+                    <input type="text" placeholder="Search MCX League">
+                </form>
+            </div>
+            <span class="cancel-btn">Cancel</span>
+
+            <div class="quick-links">
+                <h2>Quick Links</h2>
+              <ul>
+                    <li>
+                        <a href="#">MCX League FAQ</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop MCX League Store Online</a>
+                    </li>
+                    <li>
+                        <a href="#">Developer Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Learn</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Support</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+
+    </div>
+
+
+    <div class="overlay"></div>
+    <main>
+        <section class="learn-info">
+            <div>
+                <div class="title-learn">
+                    <h2></h2>
+                    <h3>Get tutorials on how to do things with tech!</h3>
+                </div>
+                <div class="link-produto">
+                    <a href="#">Learn More</a>
+                    <a class="link-produto-comprar" href="#">Continue</a>
+                </div>
+            </div>
+        </section>
+        <section class="presente">
+            <div class="texto-presente">
+                <h2>Developer Resorces</h2>
+                <h3>Get access to all the development IoT and Datacenter API's your need for free.</h3>
+                <a class="link-produto-comprar" href="#">Check it out</a>
+            </div>
+        </section>
+        <section class="games">
+            <div class="content-games">
+                <div class="texto-games">
+                    <h2>iPad</h2>
+                    <h3>Inspira. Desenha. Diverte</h3>
+                    <div>
+                        <a href="#">Saiba mais</a>
+                        <a class="link-produto-comprar" href="#">Comprar</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="info-card">
+            <section class="info-card-1">
+                <figure class="title-info-card-1">
+                    <h2>iPhone 14 Pro</h2>
+                    <h3>Pro. E além.</h3>
+                </figure>
+                <div class="link-produto">
+                    <a href="#">Saiba mais</a>
+                    <a class="link-produto-comprar" href="#">Comprar</a>
+                </div>
+            </section>
+            <section class="info-card-2">
+                <div class="title-info-card-2">
+                    <h2>AirPods Pro</h2>
+                    <h3>E refez-se o som.</h3>
+                </div>
+                <div class="link-produto">
+                    <a href="#">Saiba mais</a>
+                    <a class="link-produto-comprar" href="#">Comprar</a>
+                </div>
+            </section>
+            <section class="info-card-3">
+                <div class="title-info-card-3">
+                    <img alt="watch-series-8 Air" src="/Assets/watch-series-8-logo.webp">
+                    <h3>Um grande salto para sua saúde.</h3>
+                </div>
+                <div class="link-produto">
+                    <a href="#">Saiba mais</a>
+                    <a class="link-produto-comprar" href="#">Comprar</a>
+                </div>
+            </section>
+            <section class="info-card-4">
+                <div class="title-info-card-4">
+                    <img src="Assets/watch-ultra-logo.webp"></img>
+                    <h3>A aventura te espera.</h3>
+                </div>
+                <div class="link-produto">
+                    <a href="#">Saiba mais</a>
+                    <a class="link-produto-comprar" href="#">Comprar</a>
+                </div>
+            </section>
+        </section>
+    </main>
+    <footer class="background-footer">
+        <section class="limited-area text-footer">
+          <p>1. Some products may be in a beta phase</p>
+        </section>
+        <nav class="limited-area nav-footer">
+            <div class="boxes-nav-footer">
+                <div class="accordion">
+                    <input id="servicos" type="checkbox" class="accordion_input">
+                    <label class="accordion_label" for="servicos">
+                        <h3>Serveces</h3>
+                    </label>
+                    <div class="accordion_content">
+                        <ul class="list">
+                            <a href="#">
+                                <li>MCX League Developer</li>
+                            </a>
+                            <a href="#">
+                                <li>MCX League Search</li>
+                            </a>
+                            <a href="#">
+                                <li>MCX League Fitness (BETA)</li>
+                            </a>
+                            <a href="#">
+                                <li>MCX League Games</li>
+                            </a>
+                            <a href="#">
+                                <li>MCX League ONE</li>
+                            </a>
+                            <a href="#">
+                                <li>MCX League Cloud</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="accordion">
+                    <input id="conta" type="checkbox" class="accordion_input">
+                    <label class="accordion_label" for="conta">
+                        <h3>MCX League Account</h3>
+                    </label>
+                    <div class="accordion_content">
+                        <ul class="list">
+                            <a href="#">
+                                <li>MCX League ID</li>
+                            </a>
+                            <a href="#">
+                                <li>MCX League Gaming Profile</li>
+                            </a>
+                            <a href="#">
+                                <li>account.mcxleague.com</li>
+                            </a>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <div class="boxes-nav-footer">
+                <div class="accordion">
+                    <input id="empresas" type="checkbox" class="accordion_input">
+                    <label class="accordion_label" for="empresas">
+                        <h3>Our Games</h3>
+                    </label>
+                    <div class="accordion_content">
+                        <ul class="list">
+                            <a href="#">
+                                <li>Find your game profile</li>
+                            </a>
+                            <a href="#">
+                                <li>Discover Our Games</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="accordion">
+                    <input id="educacao" type="checkbox" class="accordion_input">
+                    <label class="accordion_label" for="educacao">
+                        <h3>MCX League For Education</h3>
+                    </label>
+                    <div class="accordion_content">
+                        <ul class="list">
+                            <a href="#">
+                                <li>Education Dashboard</li>
+                            </a>
+                            <a href="#">
+                                <li>Education Plans</li>
+                            </a>
+                            <a href="#">
+                                <li>Education For School Distrects</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="accordion">
+                    <input id="saude" type="checkbox" class="accordion_input">
+                    <label class="accordion_label" for="saude">
+                        <h3>For Business</h3>
+                    </label>
+                    <div class="accordion_content">
+                        <ul class="list">
+                            <a href="#">
+                                <li>MCX League And Business</li>
+                            </a>
+                            <a href="#">
+                                <li>Shop for Business</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="boxes-nav-footer">
+                <div class="accordion">
+                    <input id="valores_apple" type="checkbox" class="accordion_input">
+                    <label class="accordion_label" for="valores_apple">
+                        <h3>MCX League Values</h3>
+                    </label>
+                    <div class="accordion_content">
+                        <ul class="list">
+                            <a href="#">
+                                <li>Accessability</li>
+                            </a>
+                            <a href="#">
+                                <li>Education</li>
+                            </a>
+                            <a href="#">
+                                <li>Privicy</li>
+                            </a>
+                            <a href="#">
+                                <li>Enviorment</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="accordion">
+                    <input id="sobre" type="checkbox" class="accordion_input">
+                    <label class="accordion_label" for="sobre">
+                        <h3>About MCX League</h3>
+                    </label>
+                    <div class="accordion_content">
+                        <ul class="list">
+                            <a href="#">
+                                <li>Newsroom</li>
+                            </a>
+                            <a href="#">
+                                <li>Developer</li>
+                            </a>
+                            <a href="#">
+                                <li>Opertunitys and Cerrers</li>
+                            </a>
+                            <a href="#">
+                                <li>Investors</li>
+                            </a>
+                            <a href="#">
+                                <li>Our Games</li>
+                            </a>
+                            <a href="#">
+                                <li>Events</li>
+                            </a>
+                            <a href="#">
+                                <li>Contact</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+<div class="limited-area footer-local">
+            <p>Other ways to buy: <a href="#">find an MCX League Store</a> or <a href="#">MCX League Reseller</a>on your region. or call 0800-761-0867.</p>
+            <p>Apple Media Services are provided by MCX League Services.</p>
+        </div>
+         <div class="limited-area footer-copyright">
+             <div class="country">
+                 <a href="#">
+                     <p>United States</p>
+                 </a>
+             </div>
+             <div class="copyright">
+                 <p>Copyright © 2023 MCX League.</p>
+             </div>
+             <div class="links-copyright-footer">
+                 <p><a href="#">Policy Policy</a></p>
+                 <p><a href="#">Tearms Of Service</a></p>
+                 <p><a href="#">Other Leagal Information</a></p>
+                 <p><a href="#">Site Map</a></p>
+             </div>
+         </div>
+     </footer>
+     <script src="index.js"></script>
+</body>
+</html>
